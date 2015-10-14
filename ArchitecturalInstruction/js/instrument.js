71fe1597fdc706d333f539206563c7386b046eb3
rajincse <rajin.cse@gmail.com>
2015-10-14 04:10:50 -0400

    #5 instrument implemented for media image

---
+++ ArchitecturalInstruction/js/instrument.js
@@ -0,0 +1,59 @@
+
+var instrument =
+{
+        visWidth: 0, 
+        visHeight: 0,
+        url:"http://localhost:9999/apa/bla.txt?",
+        init: function( visWidth, visHeight, url)
+        {
+            this.visWidth = visWidth;
+            this.visHeight = visHeight;
+            if(url)
+            {
+                this.url = url;   
+            }
+            
+        },
+        
+        windowReshaped: function()
+        {
+            this.sendCommands(["window_" + screenX + "_" + screenY + "_" + Math.min(window.innerWidth,this.visWidth) + "_" + Math.min(window.innerHeight, this.visHeight)]);
+        }, 
+        sendCommands:function(c)
+        {
+            var s = this.url;
+        
+            for (var i=0; i<c.length; i++)
+                    s+= "command=" + c[i];
+            console.log('Sending command:'+s);
+            var xhttp = new XMLHttpRequest();
+            xhttp.open('GET', s, true);
+            xhttp.send();
+//            d3.tsv(s, function(error, data) {
+//               if (error) throw error;
+//                
+//            });
+        }
+        
+}
+
+    
+window.onmousemove = function(e){
+
+
+        var newscreenx = e.screenX - e.clientX - document.getElementsByTagName('body')[0].getBoundingClientRect().left ;
+        var newscreeny = e.screenY - e.clientY - document.getElementsByTagName('body')[0].getBoundingClientRect().top ;;
+        if (newscreenx != screenX || newscreeny != screenY){
+                screenX = newscreenx; screenY = newscreeny;
+                instrument.windowReshaped();
+        }
+        //sendCommands(["gaze_"+e.screenX + "_" + e.screenY]);
+}
+	
+window.onblur = function(){
+       instrument.sendCommands(["lostfocus"]);
+}
+	
+window.onfocus = function(){
+        instrument.sendCommands(["gainedfocus"]);
+}
--- ArchitecturalInstruction/js/main.js
+++ ArchitecturalInstruction/js/main.js
@@ -38,6 +38,7 @@ homeApp.directive("media", function ()
     
 function AppController($scope, $http)
 {
+    init();
     $scope.menuControllerData = {hoverCategoryName: "", hoverSubCategoryName: "",
         selectedCategoryName: "", selectedSubCategoryName: ""};
     $http({method: 'GET', url: "data/menu_data.json"}).
@@ -125,10 +126,52 @@ function AppController($scope, $http)
     {
         $scope.controllerData.submoduleIndex = submoduleIndex;
         $scope.controllerData.mediaIndex = 0;
+        init('subModule');
     }
     $scope.setMediaIndex = function(mediaIndex)
     {
         $scope.controllerData.mediaIndex = mediaIndex;
+        init('media');
+    }
+    function init(param)
+    {
+        if(param)
+        {
+            
+            if($scope.contentData.moduleName)
+            {
+                var currentSubModule = $scope.contentData.submodule[ $scope.controllerData.submoduleIndex];
+                var currentMedia = currentSubModule.mediaList[ $scope.controllerData.mediaIndex];
+                if(currentSubModule && currentSubModule && currentMedia.aoiData)
+                {
+                    var command =['removeAllElem'];
+                    var bigMediaImage = angular.element(query('.container div.media-image-big img'))[0].getBoundingClientRect();
+                    var rect={x:bigMediaImage.left|0, y:bigMediaImage.top|0, width:bigMediaImage.width|0, height:bigMediaImage.height|0}
+                    var scaleX = rect.width/ currentMedia.aoiData.width;
+                    var scaleY = rect.height / currentMedia.aoiData.height;
+                    var bigMediaName = currentMedia.aoiData.imageName;
+                    command.push('addElem_'+bigMediaName+'_'+rect.x+'_'+rect.y+'_'+rect.width+'_'+rect.height);
+                    for(var i=0;i<currentMedia.aoiData.aoiItemList.length;i++)
+                    {
+                        var aoi = currentMedia.aoiData.aoiItemList[i];
+                        command.push('addElem_'+aoi.name+'_'+((aoi.x * scaleX+bigMediaImage.left)|0)+'_'+((aoi.y*scaleY+bigMediaImage.top)|0)
+                                +'_'+((aoi.width*scaleX)|0)+'_'+((aoi.height*scaleY)|0));
+                    }
+                    instrument.sendCommands(command);
+                }
+                
+            }
+        }
+        else
+        {
+            var rect = document.getElementsByTagName('body')[0].getBoundingClientRect();
+            instrument.init(rect.width, rect.height);
+            console.log('init');
+        }
+    }
+    function query(command)
+    {
+        return document.querySelector(command);
     }
     
     
